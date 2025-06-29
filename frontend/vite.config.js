21cfdf16a500c93a3007f0c32c73db88ed548fae
QC2k4 <quang987xyz@gmail.com>
2025-06-29 13:00:37 +0700

    Setup Vite + React deploy to GitHub Pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Replace with your actual repo name
+const repoName = 'Chemistry_Test'
+
 export default defineConfig({
   plugins: [react()],
+  base: `/${repoName}/`,
 })
